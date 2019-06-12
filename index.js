d27fed84c12e04afdd373fef910089985d4d463b
Janette Madera <jemadera24@gmail.com>
2019-06-12 23:43:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,3 @@ function logShout(string){
 function logWhisper(string){
   console.log('hello')
 }
-function sayHiToGrandma(string){
-  if shout(string).toLowerCase {return "I can't hear you!"}
-}
